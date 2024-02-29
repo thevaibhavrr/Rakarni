769cfd8fd8c3c18384a20e8eaa8df025511dd681
vaibhav_rathore <vaibahvrathorema@gmail.com>
2024-02-29 18:04:59 +0530

    updaer user details

--- src/components/contact/delarDetails.jsx
+++ src/components/contact/delarDetails.jsx
@@ -3,22 +3,25 @@ import "../../styles/contact/delarDetails.css"
 const DealerDetails = () => {
   const dealers = [
     {
+      shop_name: "Mozaffnagar Dealer",
       name: "Mayur interior",
       firmName: " Mohd Mohasin",
       address: "Near Balaji dham, Mozaffnagar",
-      phoneNumber: "9058545024"
+      phoneNumber: "+91 9058545024"
     },
     {
+      shop_name:"Haridwar Dealer",
       name: "Lakhan Singh",
-      firmName: " Haridwar  dealer",
-      address: " Rajasthan pop",
-      phoneNumber: "9837263625",
+      firmName: " Rajasthan P.O.P Traders",
+      address: " Shop no-12, kumhar dharamshala, Jwalapur (Haridwar)",
+      phoneNumber: "+91 9837263625, +91 9837263625",
     },
     {
-      name: "Shree Balaji trading co.",
-      firmName: "Behat road Saharanpur",
-      address: "Arpit Bansal",
-      phoneNumber: "9808924046" 
+      shop_name:"Saharanpur Dealer",
+      name: "Arpit Bansal",
+      firmName: " Shree Balaji trading co. ",
+      address: "Behat road Saharanpur",
+      phoneNumber: "+91 9808924046" 
     }
   ];
 
@@ -28,7 +31,9 @@ const DealerDetails = () => {
       
       {dealers.map((dealer, index) => (
         <div className="dealer" key={index}>
-          <h3 className='dealer_name' >{dealer.name}</h3>
+          <h3 className='dealer_name' >{dealer.shop_name}</h3>
+          <p><strong>Dealer Name:</strong> {dealer.name}</p>
+
           <p><strong>Firm Name:</strong> {dealer.firmName}</p>
           <p><strong>Address:</strong> {dealer.address}</p>
           <p><strong>Phone Number:</strong> {dealer.phoneNumber}</p>
